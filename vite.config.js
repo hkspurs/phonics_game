0e37190aa725f11d9399a319aff86228ad853b21
hkspurs <hkspurs@gmail.com>
2026-07-06 15:05:36 +0000

    feat: Phase 5 - Netlify configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/phonics_game/',
+  base: './', // Use relative paths for better portability (e.g. Netlify/Vercel)
 })
